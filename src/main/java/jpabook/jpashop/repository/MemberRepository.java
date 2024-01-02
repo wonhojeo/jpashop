a41fd8110d8af8c893fb3b94afbafc2ad502cef1
wonho.jeong <jeongwh0310@gmail.com>
2024-01-02 23:54:00 +0900

    init

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -24,7 +24,7 @@ public class MemberRepository {
     }
 
     public List<Member> findAll(){
-        return em.createQuery("select m from Member", Member.class)
+        return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
 
